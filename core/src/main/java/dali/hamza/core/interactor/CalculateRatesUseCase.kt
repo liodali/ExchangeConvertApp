5dbf53f9b88583629b741c9850019d94df02473e
dali <lionelviperdali@gmail.com>
2021-05-29 15:02:35 +0100

    add calculation of rates use case

---
+++ core/src/main/java/dali/hamza/core/interactor/CalculateRatesUseCase.kt
@@ -0,0 +1,14 @@
+package dali.hamza.core.interactor
+
+import dali.hamza.domain.models.IResponse
+import dali.hamza.domain.repository.IRepository
+import dali.hamza.domain.usecase.FlowIResponseUseCase
+import kotlinx.coroutines.flow.Flow
+
+class CalculateRatesUseCase constructor(
+    private val repository: IRepository
+) : FlowIResponseUseCase<Double> {
+    override suspend fun invoke(parameter: Double?): Flow<IResponse> {
+        return repository.getListRatesCurrencies(parameter!!)
+    }
+}
