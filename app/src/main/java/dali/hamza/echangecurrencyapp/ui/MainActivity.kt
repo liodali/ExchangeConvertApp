76466c5864d23b8e13f0a2467fb2edc24900b903
dali <lionelviperdali@gmail.com>
2021-05-30 19:31:07 +0100

    add small splash screen

--- app/src/main/java/dali/hamza/echangecurrencyapp/ui/MainActivity.kt
+++ app/src/main/java/dali/hamza/echangecurrencyapp/ui/MainActivity.kt
@@ -1,21 +1,22 @@
 package dali.hamza.echangecurrencyapp.ui
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.animation.Crossfade
 import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.runtime.CompositionLocalProvider
-import androidx.compose.runtime.compositionLocalOf
+import androidx.compose.runtime.*
 import androidx.lifecycle.lifecycleScope
 import dagger.hilt.android.AndroidEntryPoint
 import dali.hamza.core.common.SessionManager
 import dali.hamza.echangecurrencyapp.ui.compose.page.Home
+import dali.hamza.echangecurrencyapp.ui.compose.page.SplashScreen
 import dali.hamza.echangecurrencyapp.ui.compose.theme.ExchangeCurrencyAppTheme
 import dali.hamza.echangecurrencyapp.ui.dialog.DialogCurrenciesFragment
 import dali.hamza.echangecurrencyapp.viewmodel.MainViewModel
 import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.withContext
 import javax.inject.Inject
@@ -41,13 +42,35 @@ class MainActivity : AppCompatActivity(), DialogCurrenciesFragment.DialogCurrenc
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            var pageInit by remember {
+                mutableStateOf("splash")
+            }
+
+            // This will always refer to the latest onTimeout function that
+            // LandingScreen was recomposed with
+            val moveToHome by rememberUpdatedState {
+                pageInit = "page1"
+            }
+
+            // Create an effect that matches the lifecycle of LandingScreen.
+            // If LandingScreen recomposes, the delay shouldn't start again.
+            LaunchedEffect(true) {
+                delay(200)
+                moveToHome()
+            }
             CompositionLocalProvider(mainViewModelComposition provides viewModel) {
                 ExchangeCurrencyAppTheme {
-                    Home(
-                        openFragment = {
-                            openCurrenciesSelectionBottomSheet()
+                    Crossfade(targetState = pageInit) { page ->
+                        when (page) {
+                            "splash" -> SplashScreen()
+                            else ->
+                                Home(
+                                    openFragment = {
+                                        openCurrenciesSelectionBottomSheet()
+                                    }
+                                )
                         }
-                    )
+                    }
                 }
             }
 
---
+++ app/src/main/java/dali/hamza/echangecurrencyapp/ui/compose/page/SplashPage.kt
@@ -0,0 +1,31 @@
+package dali.hamza.echangecurrencyapp.ui.compose.page
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.Icon
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import dali.hamza.echangecurrencyapp.R
+
+@Composable
+fun SplashScreen() {
+    Column(
+        Modifier
+            .fillMaxHeight()
+            .fillMaxWidth(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Icon(
+            painter = painterResource(id = R.drawable.ic_exchange),
+            contentDescription = null,
+            tint = Color.Unspecified
+        )
+    }
+}
