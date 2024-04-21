34101266b8f4647df671b6e609ab4b833a757006
liodali <16631886+liodali@users.noreply.github.com>
2024-04-21 19:51:51 +0200

    * create Rates Page

---
+++ app/src/main/java/dali/hamza/echangecurrencyapp/ui/compose/page/Rates.kt
@@ -0,0 +1,27 @@
+package dali.hamza.echangecurrencyapp.ui.compose.page
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import dali.hamza.echangecurrencyapp.ui.compose.component.ExchangesRatesGrid
+import dali.hamza.echangecurrencyapp.ui.compose.component.HeaderHomeCompose
+import dali.hamza.echangecurrencyapp.ui.compose.component.SpacerHeight
+
+@ExperimentalComposeUiApi
+@Composable
+fun RatesPageCompose(
+    modifier: Modifier, openFragment: () -> Unit
+) {
+
+    Column(modifier = modifier) {
+        HeaderHomeCompose(
+            openFragment = openFragment
+        )
+        SpacerHeight(
+            height = 24.dp
+        )
+        ExchangesRatesGrid()
+    }
+}
