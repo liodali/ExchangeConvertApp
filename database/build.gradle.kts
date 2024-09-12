9a0b8b39f9a099c67f1dd5075873056424ae0ab9
liodali <16631886+liodali@users.noreply.github.com>
2024-09-13 01:48:51 +0200

    * update dependencies

--- database/build.gradle.kts
+++ database/build.gradle.kts
@@ -35,7 +35,7 @@ android {
 
 dependencies {
     val roomVersion = "2.6.1"
-    val pagingVersion = "3.3.0"
+    val pagingVersion = "3.3.2"
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.room:room-runtime:$roomVersion")
     // optional - Kotlin Extensions and Coroutines support for Room
@@ -46,7 +46,7 @@ dependencies {
     implementation("androidx.room:room-paging:$roomVersion")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
 
     implementation(project(":domain"))
 }
